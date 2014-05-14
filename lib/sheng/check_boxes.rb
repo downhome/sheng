cc45dda7b4c59aeed4b10e77cf2d24f5a03ab382
Maher Hawash <gmhawash@gmail.com>
2014-05-14 11:59:27 -0700

    II_4771: Fix issue with checkboxes.
    - Add tests starting from XML.
    - Scope path checkbox lookup to form data (ffData) and only select checkBoxes.

--- lib/sheng/check_boxes.rb
+++ lib/sheng/check_boxes.rb
@@ -1,18 +1,19 @@
 module Sheng
   class CheckBoxes < Sheng::ReplacerBase
     def replace params, xml
-      params.each do |k, v|
-        find_elements("//w:name[contains(@w:val, '#{k}')]", xml).each do |element|
-          value = v ? "1" : "0"
-
-          new_node = Nokogiri::XML::Node.new('checked', xml)
-          new_node.set_attribute("val", value)
-
-          element.parent.search("//w:default").first.attribute("val").value = value
-          element.parent.search("//w:checkBox").first.add_child(new_node)
+      params.each do |checkbox_name, checked|
+        find_check_boxes(checkbox_name, xml).each do |element|
+          default_attribute = element.parent.search(".//w:default").first.attribute("val")
+          default_attribute.value = checked ? "1" : "0"
         end
       end
       xml
+    end
+
+    private
+    def find_check_boxes(checkbox_name, xml)
+      find_elements("//w:ffData/w:name[contains(@w:val, '#{checkbox_name}')]", xml)
+        .reject {|e| e.parent.xpath('.//w:checkBox').empty? }
     end
   end
 end
---
+++ spec/libs/check_boxes_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+describe Sheng::CheckBoxes do
+  let(:input_hash) { Sheng::Support.symbolize_keys(JSON.parse(input_json)) }
+  let(:params) { input_hash[:check_boxes] }
+  let(:input_json) { File.open("#{SPEC_ROOT}/fixtures/check_boxes/input.json").read }
+  let(:input_xml) { Nokogiri::XML(File.read(fixture_path('check_boxes/input_document.xml'))) }
+  let(:output_xml) { File.read(fixture_path('check_boxes/output_document.xml')) }
+
+  describe '#replace' do
+    before :each do
+      @check_boxes_xml = described_class.new.replace(params, input_xml)
+    end
+
+    it "should replace all checkboxes in the document (xml)" do
+      get_unmerged_fields(@check_boxes_xml).should == []
+    end
+
+    it "generates output xml with replaced values" do
+      expect(@check_boxes_xml.to_xml).to eq output_xml
+    end
+
+    it "should assume defined strings as 'true'" do
+      input_hash['designee_3p'] = 'really true'
+      expect(@check_boxes_xml.to_xml).to eq output_xml
+    end
+
+    it "should assume nil is 'false'" do
+      input_hash['designee_co'] = nil
+      expect(@check_boxes_xml.to_xml).to eq output_xml
+    end
+  end
+end

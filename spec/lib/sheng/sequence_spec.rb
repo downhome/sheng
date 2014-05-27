5109fc431517fd30da195352785bd178a3c8b1d6
Ravi Gadad <ravi@renewfund.com>
2014-05-27 13:14:04 -0700

    Test for regular sequences

--- spec/lib/sheng/sequence_spec.rb
+++ spec/lib/sheng/sequence_spec.rb
@@ -1,5 +1,24 @@
 describe Sheng::Sequence do
   describe '#interpolate' do
+    it 'duplicates template and replaces mergefields for each array member' do
+      dataset = Sheng::DataSet.new({
+        :library => {
+          :books => [
+            { :title => 'A Radish Summer', :scent => 'totally rad' },
+            { :title => 'Elephants Are Not Your Friend', :scent => 'stinky' }
+          ]
+        }
+      })
+
+      xml = xml_fragment('input/sequence')
+      merge_field = Sheng::MergeField.new(xml.xpath("//w:fldSimple[contains(@w:instr, 'start:')]").first)
+      subject = described_class.new(merge_field)
+      subject.interpolate(dataset)
+      expect(subject.xml).to be_equivalent_to xml_fragment('output/sequence')
+    end
+  end
+
+  describe '#interpolate' do
     it 'can handle table-based sequences with multiple rows' do
       dataset = Sheng::DataSet.new({
         :meals => [

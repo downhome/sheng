9ed152283dcb9e8dab815cf84f4f9a78c7ae3000
Krishan Sharma <ksharma@renewfinancial.com>
2016-06-14 12:52:48 -0700

    adding a private method for inserting commas
    Signed-off-by: Krishan Sharma <ksharma@renewfinancial.com>

--- lib/sheng/filters/currency_formatting_filter.rb
+++ lib/sheng/filters/currency_formatting_filter.rb
@@ -10,11 +10,18 @@ module Sheng
         integer, fractional = ("%00.2f" % value).split(".")
 
         if integer.length > 3
-          integer.reverse!.gsub!(/(\d{3})(?=\d)/, '\\1,').reverse!
+          insert_commas(integer)
         end
 
         "#{arguments.first}#{integer}.#{fractional}"
       end
+
+      private
+
+      def insert_commas(integer)
+        integer.reverse!.gsub!(/(\d{3})(?=\d)/, '\\1,').reverse!
+      end
+
     end
   end
 end

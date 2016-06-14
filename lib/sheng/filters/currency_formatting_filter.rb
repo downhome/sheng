486f4360b4c4f3b47d78820bdc208e8adaae0a6e
Krishan Sharma <ksharma@renewfinancial.com>
2016-06-14 12:06:14 -0700

    SI-86 adding an edge case for the bug caught and adding rspec
    Signed-off-by: Krishan Sharma <ksharma@renewfinancial.com>

--- lib/sheng/filters/currency_formatting_filter.rb
+++ lib/sheng/filters/currency_formatting_filter.rb
@@ -8,7 +8,11 @@ module Sheng
       def filter(value)
         return value unless Sheng::Support.is_numeric?(value)
         integer, fractional = ("%00.2f" % value).split(".")
-        integer.reverse!.gsub!(/(\d{3})(?=\d)/, '\\1,').reverse!
+
+        if integer.length > 3
+          integer.reverse!.gsub!(/(\d{3})(?=\d)/, '\\1,').reverse!
+        end
+
         "#{arguments.first}#{integer}.#{fractional}"
       end
     end
